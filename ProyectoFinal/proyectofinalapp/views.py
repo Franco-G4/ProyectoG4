a262dbb0f09f2ababcfc1a262c1d38e1ac2367f5
Franco-G4 <78695624+Franco-G4@users.noreply.github.com>
2021-02-28 18:04:27 -0300

    correo terminado

--- ProyectoFinal/proyectofinalapp/views.py
+++ ProyectoFinal/proyectofinalapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 
@@ -22,7 +23,19 @@ def nosotros(request):
 
     return render(request, "proyectofinalapp/nosotros.html")
 
+#formulario de contacto
 def contacto(request):
+    
+    if request.method=="POST":
+
+        subject=request.POST["Nombre"]
+        #asunto=request.POST["Asunto"]
+        message=request.POST["Mensaje"] + " " + request.POST["Email"]
+        email_from=settings.EMAIL_HOST_USER
+        recipient_list=["pruebasvariasprogramacion@gmail.com"]
+        send_mail(subject, message, email_from, recipient_list)
+
+        return render(request, "proyectofinalapp/carrusel.html")
 
     return render(request, "proyectofinalapp/contacto.html")
 
