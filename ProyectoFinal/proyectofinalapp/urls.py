81b6eda2229f5febf882cbb24286ec3157f04a6c
Franco-G4 <78695624+Franco-G4@users.noreply.github.com>
2021-03-08 16:42:27 -0300

    renegando con heroku

--- ProyectoFinal/proyectofinalapp/urls.py
+++ ProyectoFinal/proyectofinalapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from proyectofinalapp import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('servicios/', views.servicios, name="servicios"),
